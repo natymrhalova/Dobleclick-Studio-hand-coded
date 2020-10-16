20e4123924d5a6bffc016e12c6fbbcf9a6eb6b77
Natálie Mrhalová <62668294+natymrhalova@users.noreply.github.com>
2020-10-16 09:18:30 +0200

    Add files via upload

---
+++ 300x600/README.md
@@ -0,0 +1,2 @@
+# Doubleclick-Studio-hand-coded
+300x600 Hand coded banner for doubleclick studio with dynamic bindings to product feed 
---
+++ 300x600/Studio_generated _code.js
@@ -0,0 +1,73 @@
+//Sem patří kód ze Studia
+//Začátek kódu vygenerovaného ve Studiu
+//Za tenhle komentář vlož kód ze Studia, co se ti vygeneroval v nastavení dynamického profilu
+Enabler.setProfileId(10552963);
+var devDynamicContent = {};
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data = [{}, {}, {}, {}];
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0]._id = 0;
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0].id = ["EAT1136l"];
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0].Item_title = "Philips OneBlade QP2520\/30 + 1 tartal\u00E9k penge";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0].Logo = {};
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0].Logo.Url = "https://i.alza.cz/Foto/ImgGalery/Image/Philips_cmyk.png";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0].Final_URL = {};
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0].Final_URL.Url = "https://www.alza.hu/philips-oneblade-qp252030-d4847997.htm";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0].Image_URL = {};
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0].Image_URL.Url = "https://admin.alza.cz/External/ImgCom.ashx?fd=f4&cd=EAT1136l";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0].Sale_Price = "13490 Ft";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0].Lowest_Price = "13490 Ft";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0].Availability = "#N\/A";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0].Brand = ["Philips"];
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[1].id = ["EAT1136m"];
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[1].Item_title = "Philips OneBlade Pro QP6510\/20";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[1].Logo = {};
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[1].Logo.Url = "https://i.alza.cz/Foto/ImgGalery/Image/Philips_cmyk.png";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[1].Final_URL = {};
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[1].Final_URL.Url = "https://www.alza.hu/philips-oneblade-pro-qp651020-d4847998.htm";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[1].Image_URL = {};
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[1].Image_URL.Url = "https://admin.alza.cz/External/ImgCom.ashx?fd=f4&cd=EAT1136m";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[1].Sale_Price = "20277 Ft";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[1].Lowest_Price = "20277 Ft";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[1].Availability = "#N\/A";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[1].Brand = ["Philips"];
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[2].id = ["EAT1136n"];
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[2].Item_title = "Philips OneBlade Pro QP6520\/20";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[2].Logo = {};
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[2].Logo.Url = "https://i.alza.cz/Foto/ImgGalery/Image/Philips_cmyk.png";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[2].Final_URL = {};
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[2].Final_URL.Url = "https://www.alza.hu/philips-oneblade-pro-qp652020-d4847999.htm";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[2].Image_URL = {};
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[2].Image_URL.Url = "https://admin.alza.cz/External/ImgCom.ashx?fd=f4&cd=EAT1136n";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[2].Sale_Price = "24375 Ft";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[2].Lowest_Price = "24375 Ft";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[2].Availability = "#N\/A";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[2].Brand = ["Philips"];
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[3].id = ["EAT1136o"];
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[3].Item_title = "Philips OneBlade QP230\/50 tartal\u00E9k penge 2 db";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[3].Logo = {};
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[3].Logo.Url = "https://i.alza.cz/Foto/ImgGalery/Image/Philips_cmyk.png";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[3].Final_URL = {};
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[3].Final_URL.Url = "https://www.alza.hu/philips-oneblade-qp23050-tartalek-penge-2-db-d4848000.htm";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[3].Image_URL = {};
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[3].Image_URL.Url = "https://admin.alza.cz/External/ImgCom.ashx?fd=f4&cd=EAT1136o";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[3].Sale_Price = "7990 Ft";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[3].Lowest_Price = "7990 Ft";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[3].Availability = "#N\/A";
+devDynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[3].Brand = ["Philips"];
+Enabler.setDevDynamicContent(devDynamicContent);
+
+//konec kódu vygenerovaného ve Studiu
+
+window.onload = function() {
+    if (Enabler.isInitialized()) {
+      enablerInitHandler();
+    } else {
+      Enabler.addEventListener(studio.events.StudioEvent.INIT, enablerInitHandler);
+    }
+  
+    function enablerInitHandler() {
+
+        document.getElementById('obrazekProduktu0').src = dynamicContent.HU_DynaRMK_Philips_Domc_elektro_Vstupn_data[0].Image_URL.Url;
+      
+    };
+  
+};        
